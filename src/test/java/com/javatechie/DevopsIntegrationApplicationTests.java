9a6b3fe66ef1fd51a949987e79f43dde5ac4dd1a
UX506983 <UX506983@DESKTOP-FIFEHSG>
2023-03-04 16:21:13 +0530

    Commit test case

--- src/test/java/com/javatechie/DevopsIntegrationApplicationTests.java
+++ src/test/java/com/javatechie/DevopsIntegrationApplicationTests.java
@@ -1,13 +1,21 @@
 package com.javatechie;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class DevopsIntegrationApplicationTests {
+	
+	public static final Logger logger = LoggerFactory.getLogger(DevopsIntegrationApplicationTests.class);
 
 	@Test
 	void contextLoads() {
+		logger.info("Test case executing...");
+		assertEquals(4, 2+2);
 	}
 
 }
